25a30d8dc8af817c1cd11e097242421b2afadf99
melon <momobp114@gmail.com>
2022-08-14 15:25:10 +0700

    Decouple code

--- src/Flock.java
+++ src/Flock.java
@@ -2,8 +2,12 @@ import java.util.ArrayList;
 
 public class Flock implements Quackable{
     private ArrayList<Quackable> quackers = new ArrayList<>();
+    private Quackable leader;
 
     public void add(Quackable quacker) {
+        if(this.quackers.isEmpty()) {
+            setLeader(quacker);
+        }
         quackers.add(quacker);
     }
 
@@ -11,12 +15,20 @@ public class Flock implements Quackable{
     public void quack() {
         for(int i = 0; i < quackers.size(); i++) {
             if(i == 0) {
-                for(int j = 0; j < 3; j++) {
-                    System.out.print("I'm the leader!: ");
-                    quackers.get(i).quack();
-                }
+                leaderQuack();
             }
             quackers.get(i).quack();
         }
+    }
+
+    private void leaderQuack() {
+        for(int j = 0; j < 3; j++) {
+            System.out.print("I'm the leader!: ");
+            this.leader.quack();
+        }
+    }
+
+    private void setLeader(Quackable leader) {
+        this.leader = leader;
     }
 }
